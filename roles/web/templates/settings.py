92030b1f1ef92bfc536cf5772182bd56a5810760
Peter Christensen <tessercat@pakaa.net>
2021-04-12 18:18:08 -0600

    Refactor for conference.
    Change pbx version to dev.
    Rename coturn -> stun.
    Move firewall port from vars to locals.
    Change nginx verto endpoint.
    Use stack-vars webroot var.
    Remove a .pyc file.

--- roles/web/templates/settings.py
+++ roles/web/templates/settings.py
@@ -2,10 +2,10 @@
     'ADMINS': (
         ('PBX admin }}', '{{ admin_email }}'),
     ),
-    'COTURN_LISTENING_PORT': {{ coturn_listening_port }},
-    'FIREWALL_API_PORT': {{ firewall_api_port }},
     'PBX_HOSTNAME': '{{ pbx_hostname }}',
     'SERVER_EMAIL': 'noreply@{{ pbx_hostname }}',
     'TIME_ZONE': '{{ timezone }}',
+    'FIREWALL_PORT': {{ firewall_port }},
+    'STUN_PORT': {{ stun_port }},
     'VERTO_PORT': {{ freeswitch_verto_port }},
 }
