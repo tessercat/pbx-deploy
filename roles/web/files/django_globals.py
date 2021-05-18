1678c452ee2c6104dc6e4d55255864c8aefcf076
Peter Christensen <tessercat@pakaa.net>
2021-05-18 14:08:27 -0600

    Split settings into globals and ports.

---
+++ roles/web/files/django_globals.py
@@ -0,0 +1,31 @@
+{
+    'ADMINS': (
+        ('PBX admin', 'peter@puptel.net'),
+    ),
+    'ALLOWED_HOSTS': (
+        'pakaa.net',
+        'localhost',
+    ),
+    'INSTALLED_APPS': [
+        'django_prometheus',
+        'common.apps.CommonConfig',
+        'fsapi.apps.FsapiConfig',
+        'configuration.apps.ConfigurationConfig',
+        'directory.apps.DirectoryConfig',
+        'dialplan.apps.DialplanConfig',
+        'verto.apps.VertoConfig',
+        'sofia.apps.SofiaConfig',
+        'gateway.apps.GatewayConfig',
+        'intercom.apps.IntercomConfig',
+        'intercom_dialplan.apps.IntercomDialplanConfig',
+        # 'conference.apps.ConferenceConfig',
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+    ],
+    'SERVER_EMAIL': 'noreply@pakaa.net',
+    'TIME_ZONE': 'America/Edmonton',
+}
---
+++ roles/web/templates/ports.py
@@ -0,0 +1,7 @@
+{
+    'firewall': {{ firewall_port }},
+    'rtp_start': {{ freeswitch_rtp_port_start }},
+    'rtp_end': {{ freeswitch_rtp_port_end }},
+    'stun': {{ stun_port }},
+    'verto': {{ freeswitch_verto_port }},
+}
